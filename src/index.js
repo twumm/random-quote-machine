d8e64331c3ab1553122479d7de8eb0d88025c943
Martin Twum Mensah <martint.mensah@gmail.com>
2018-08-17 22:23:33 +0000

    feat: Fetch quote from json and update the quoteText and quoteAuthor state.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import quotes from './quotesData'
+import quotesData from './quotesData'
 
+const quotesDataLength = quotesData.quotes.length + 1
+// let quote = quotesData.quotes[Math.floor(Math.random() * quotesDataLength)]
+// console.log(Math.random() * Math.floor(quotesDataLength))
+// console.log(Math.floor(Math.random() * quotesDataLength))
+// console.log(quote)
 
 class Main extends React.Component {
   constructor(props) {
@@ -30,12 +35,45 @@ class QuoteBox extends React.Component {
 }
 
 class Quotes extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quoteText: quotesData.quotes[Math.floor(Math.random() * quotesDataLength)].quote,
+      quoteAuthor: quotesData.quotes[Math.floor(Math.random() * quotesDataLength)].author
+    }
+    this.getQuote = this.getQuote.bind(this)
+  }
+
+  getQuote() {
+    // Get random quote and update the state.
+    let quote = quotesData.quotes[Math.floor(Math.random() * quotesDataLength)]
+    this.setState({
+      quoteText: quote.quote,
+      quoteAuthor: quote.author
+    })
+  }
+
   render() {
     return (
       <div>
-        <h4 id="text">dhjhkj</h4>
-        <h6 id="author">rtdf</h6>
-        <button className="btn btn-success" id="new-quote" type="submit">New Quote</button>
+        <h4
+          id="text"
+        >
+          {this.state.quoteText}
+        </h4>
+        <h6
+          id="author"
+        >
+          {this.state.quoteAuthor}
+        </h6>
+        <button
+          className="btn btn-success"
+          onClick={this.getQuote}
+          id="new-quote"
+          type="submit"
+        >
+          New Quote
+        </button>
         <ShareQuote />
       </div>
     )
